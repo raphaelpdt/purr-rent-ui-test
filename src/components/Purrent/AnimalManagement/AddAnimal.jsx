46dec890103132680c1b73afe519e3ce36b40abb
g6o1b <mina.kong_22@hotmail.com>
2019-06-14 17:07:09 -0700

    add animal form updated using state

--- src/components/Purrent/AnimalManagement/AddAnimal.jsx
+++ src/components/Purrent/AnimalManagement/AddAnimal.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Button, Form, Grid, Checkbox, Header, Select } from 'semantic-ui-react';
+import { Button, Form, Grid, Checkbox, Header, Select, Dropdown } from 'semantic-ui-react';
+import { nullableTypeAnnotation } from '@babel/types';
 
 const animalOptions = [
     {
@@ -47,42 +48,107 @@ const dietOptions = [
     }
 ]
 
+const blidOptions = [
+    { key: 69, value: 69, text: 69 },
+    { key: 70, value: 70, text: 70 },
+    { key: 71, value: 71, text: 71 },
+    { key: 72, value: 72, text: 72 },
+    { key: 73, value: 73, text: 73 },
+]
+
 
 export default class AddAnimal extends Component {
+    state = {
+        animalid: null,
+        animaltype: null,
+        diet: null,
+        blid: null,
+        clinid: null,
+        packageid: null,
+
+
+        
+    }
+
+    tempState = {
+        animalid: null,
+        animaltype: null,
+        diet: null
+        
+    }
+
+    handleTextChange = e => {
+        // console.log(e.target.value)
+        // console.log(e.target.name)
+        this.setState({[e.target.name]: e.target.value}, 
+            console.log(this.state))      
+    }
+    handleDropdownChange = (e, data) => {
+        // console.log('dd: ' + value)
+        // console.log(data)
+        // console.log(data.value)
+        this.setState({[data.name] : data.value }, console.log(this.state))
+
+    }
+
     render() {
+        const {
+            value
+        } = this.state
+
         return (
     <Grid>
         <Grid.Row centered>
             <Header/>
             <Form>
                 <Form.Field>
-                    <label>Enter a 3 Digit AnimalID:</label>
-                    <input placeholder='Animal ID' name='animalid'/>
+                    <label htmlFor='animalid'>Enter a 3 Digit AnimalID:</label>
+                    <input 
+                        type='text' 
+                        placeholder='Animal ID' 
+                        name='animalid' 
+                        onChange={this.handleTextChange}/>
                 </Form.Field>
                 <Form.Field>
-                    <label>Type of Animal:</label>
-                    <Select placeholder='Select type of animal' options={animalOptions}/>
+                    <label htmlFor='animaltype'>Type of Animal:</label>
+                    <Dropdown 
+                        type='text' 
+                        placeholder='Select type of animal'
+                        name='animaltype'
+                        options={animalOptions}
+                        onChange={this.handleDropdownChange}
+                        />
                   </Form.Field>
                   <Form.Field>
-                     <label> Diet:</label>
-                     <Select placeholder='Select type of diet' options={dietOptions}/>
+                     <label htmlFor='diet'> Diet:</label>
+                     <Dropdown 
+                        type='string' 
+                        placeholder='Select type of diet' 
+                        options={dietOptions} 
+                        name='diet'
+                        onChange={this.handleDropdownChange}/>
                   </Form.Field>
-                  <Form.Field>
-                       <label>Home Fungeon:</label>
-                       <Select placeholder='Select Fungeon ID' options={null}/>
-                   </Form.Field>
-                   {/* <Form.Field>
-                       <label>Clinic:</label>
-                       <Select placeholder='autofill dependent on animal type' options={null}/>
+                    <Form.Field>
+                        <label>Home Fungeon: (options should be queried from db)</label>
+                        <Dropdown 
+                            placeholder='Select a fungeon location' 
+                            options={blidOptions} 
+                            name='blid'
+                            onChange={this.handleDropdownChange}/>
+                    </Form.Field>
+                   <Form.Field>
+                       <label>Clinic: (options should be queried from db)</label>
+                       <Dropdown placeholder='Select a clinic' options={null} name='clinid'/>
                    </Form.Field>
                    <Form.Field>
-                       <label>Care Package:</label>
-                       <Select placeholder='autofill dependent on ......' options={null}/>
-                   </Form.Field> */}
-                   <Button color='orange'> Submit </Button>
+                       <label>Care Package: (options should be queried from db)</label>
+                       <Dropdown placeholder='Select a care package' options={null} name='packageid'/>
+                   </Form.Field>
+                   <Button color='orange' onClick={this.setState(value, console.log(this.state))}> Submit </Button>
             </Form>
         </Grid.Row>
     </Grid>
         );
     };
+
 }
