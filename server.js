03860ab6b096ca6198ba499b53d2545428bee267
g6o1b <mina.kong_22@hotmail.com>
2019-06-19 00:59:14 -0700

    setupproxy

--- server.js
+++ server.js
@@ -68,26 +68,45 @@ app.post('/animals', function (req, res) {
     })
 })
 
-app.post('/api/customers/signup', function(req, res) {
+app.post('/api/customers/signup', function (req, res) {
     let custid = req.body.custid
     let name = req.body.name
-    let address = req.body.address 
+    let address = req.body.address
     let pnum = req.body.pnum
+    pool.connect((err, db, done) => {
+        if (err) {
+            console.error('error fetching data\n' + err)
+            res.status(500).send()
+        }
+        else {
+            db.query(`INSERT INTO Customer VALUES(${custid}, ${name}, ${address}, ${pnum}`),
+                (err, table) => {
+                    if (err)
+                        return res.status(500).send(err)
+                    else {
+                        // console.table(table)
+                        console.log('connection success')
+                        res.status(200).send()
 
-    console.log('value from request body: '+custid)
-    console.log('value from request body: '+name)
-    console.log('value from request body: '+address)
-    console.log('value from request body: '+pnum)
-    
-    res.status(200).send()
+                    }
+                }
+        }
+    })
+
+    // console.log('value from request body: '+custid)
+    // console.log('value from request body: '+name)
+    // console.log('value from request body: '+address)
+    // console.log('value from request body: '+pnum)
+
+    // res.status(200).send()
 })
-/* hi baby */ 
+/* hi baby */
 
 app.post('/api/customers/login', function (req, res) {
     console.log('request body: ' + req.body.custid);
     let custid = req.body.custid
-    console.log('connection success') 
-    
+    console.log('connection success')
+
     // res.status(200).send(custid)
     pool.connect((err, db, done) => {
         if (err) {
@@ -98,11 +117,11 @@ app.post('/api/customers/login', function (req, res) {
         else {
             db.query(`select * from customer where custid=${custid}`, (err, table) => {
                 if (err)
-                return res.send(500, err)
-                    // res.status(500).send(err)
+                    return res.send(500, err)
+                // res.status(500).send(err)
                 else {
                     // console.table(table)
-                    console.log('connection success') 
+                    console.log('connection success')
                     res.status(200).send(custid)
 
                 }
@@ -149,10 +168,10 @@ app.get('/animals', (req, res) => {
             res.json(table)
         })
         db.query("select * from animal", (err, table) => {
-                if (err)
-                    return console.log(err)
-                res.json(table)
-            })
+            if (err)
+                return console.log(err)
+            res.json(table)
+        })
     })
 })
 
--- src/components/Customer/Login.js
+++ src/components/Customer/Login.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import { Form, Button, Grid, Header } from 'semantic-ui-react';
 import { Link } from "react-router-dom";
@@ -7,53 +7,48 @@ import { async } from 'q';
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       custid: ''
     }
   }
-    getID = (e) => {
-        console.log(e.target.name)
-        console.log(e.target.value)
-        this.setState({ [e.target.name]: e.target.value });
-    }
+  getID = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    makeFetch = async (data) => {
-        const { history } = this.props
-        const res = await axios.post('/api/customers/login', data);
-        // console.log(this.props);
-        // console.log('1 '+res.data);
-        // console.log(res.status);
-        // console.log(res.data)
-        this.setState({custid: res.data})
-        // console.log(this.state.custid);
-        // console.log(this.state.test); 
-        if (res.status===200){
-          history.push('/customer', res.data)
-        }
-    }
-    
-    render(){
-        return(
-        <Grid>
+  makeFetch = async (data) => {
+    const { history } = this.props
+    const res = await axios.post('/api/customers/login', data);
+    this.setState({ custid: res.data })
+    if (res.status === 500) {
+      alert("some thing is wrong, try again")
+    } else
+      if (res.status === 200) {
+        history.push('/customer', res.data)
+      }
+  }
+
+  render() {
+    return (
+      <Grid>
         <Header>
 
         </Header>
-            <Grid.Row centered>
-                <Form>
-                <Form.Field>
-                <label htmlFor="custid">Enter your 3 Digit Number CustID:</label>
-                <input type='text' name='custid' placeholder='Customer ID' onChange={this.getID}/>
-                </Form.Field>
+        <Grid.Row centered>
+          <Form>
+            <Form.Field>
+              <label htmlFor="custid">Enter your 3 Digit Number CustID:</label>
+              <input type='text' name='custid' placeholder='Customer ID' onChange={this.getID} />
+            </Form.Field>
 
-                {/* <Link to={{pathname: '/customer', state: this.state.custid}}>
+            {/* <Link to={{pathname: '/customer', state: this.state.custid}}>
                 Login */}
-                <Button onClick={() => this.makeFetch(this.state)} > Login </Button>
-                {/* </Link> */}
-                {/* <Form success>
+            <Button onClick={() => this.makeFetch(this.state)} > Login </Button>
+            {/* </Link> */}
+            {/* <Form success>
                 <Message success header='Form Completed' content="You're all signed up!" /> */}
-                </Form> 
-            </Grid.Row>
-        </Grid>
-        );
-    };
+          </Form>
+        </Grid.Row>
+      </Grid>
+    );
+  };
 }
